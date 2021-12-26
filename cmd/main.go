d0d54bd75d7183ae660002e69e10ced5ac49bffc
Paul Kuruvilla <rohitpaulk@gmail.com>
2021-12-26 15:39:21 +0000

    add one nr_uarts

--- cmd/main.go
+++ cmd/main.go
@@ -103,7 +103,7 @@ func runVMM(ctx context.Context, rootFSPath string) error {
 			},
 		},
 		KernelImagePath: "/root/firecracker-resources/vmlinux.bin",
-		KernelArgs:      "ro console=ttyS0 noapic reboot=k panic=1 tsc=reliable 8250.nr_uarts=0 ipv6.disable=1 pci=off init=/init",
+		KernelArgs:      "ro console=ttyS0 noapic reboot=k panic=1 tsc=reliable 8250.nr_uarts=1 ipv6.disable=1 pci=off init=/init",
 		LogLevel:        "Debug",
 		LogPath:         "/tmp/firecracker-logs",
 		MachineCfg: models.MachineConfiguration{
