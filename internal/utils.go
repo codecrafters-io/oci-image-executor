63fcaddcac027777611b0fca30044a3e4d5e9c56
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-01-16 01:50:39 +0000

    add 'ran command' log

--- internal/utils.go
+++ internal/utils.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 )
@@ -9,6 +10,10 @@ func RunCommandAndLogToStderr(name string, args ...string) error {
 	cmd := exec.Command(name, args...)
 	cmd.Stdout = os.Stderr
 	cmd.Stderr = os.Stderr
+
+	defer func() {
+		fmt.Printf("Ran command %s %s\n", name, args)
+	}()
 
 	return cmd.Run()
 }
